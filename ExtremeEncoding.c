a1a649cb8e55e5e084b95b1aa7ca5c695e04be4e
Vinayak Suresh <vinayaksuresh.india@gmail.com>
2017-01-03 17:27:29 +0530

    Jan 3
    A line of code under the function decodeIntergerB(),
    x = x&~(15);
    is not required. Hence removed the specific line of code!

--- ExtremeEncoding.c
+++ ExtremeEncoding.c
@@ -51,37 +51,36 @@ int decodeIntegerA(int x){
 }
 
 int decodeIntegerB(int x) {
-	x = x&~(15);
 	x = x>>16;
 	return x;
 }
 
 int main() {
-    long long int T = 0,n = 0,i = 0, caseCount = 0;
+  long long int T = 0,n = 0,i = 0, caseCount = 0;
 	scanf("%d",&T);
-	if (T >= 1 && T <=100){
-	while(T>0) {
-	    caseCount++;
-	    i = 0;
-	    n = 0;
-	    scanf("%d",&n);
-	    long long int decodedA[n], decodedB[n], encodedArray[n];
-	    for(i=0;i<n;i++) {
-	        scanf("%d",&encodedArray[i]);
-	        decodedA[i] = decodeIntegerA(encodedArray[i]);
-	        decodedB[i] = decodeIntegerB(encodedArray[i]);
-	    }
-	    printf("Case %d:\n", caseCount);
-	    for(i=0;i<n;i++) {
-	        printf("%d ", decodedA[i]);
-	    }
-	    printf("\n");
-	    for(i=0;i<n;i++) {
-	        printf("%d ", decodedB[i]);
-	    }
-	    printf("\n\n");
-	    T--;
-	}
+  if (T >= 1 && T <=100){
+	   while(T>0) {
+	       caseCount++;
+	       i = 0;
+	       n = 0;
+	       scanf("%d",&n);
+	       long long int decodedA[n], decodedB[n], encodedArray[n];
+	       for(i=0;i<n;i++) {
+	          scanf("%d",&encodedArray[i]);
+	          decodedA[i] = decodeIntegerA(encodedArray[i]);
+	          decodedB[i] = decodeIntegerB(encodedArray[i]);
+	       }
+	       printf("Case %d:\n", caseCount);
+	       for(i=0;i<n;i++) {
+	          printf("%d ", decodedA[i]);
+	       }
+	       printf("\n");
+	       for(i=0;i<n;i++) {
+	           printf("%d ", decodedB[i]);
+	       }
+	       printf("\n\n");
+	       T--;
+       }
     }
 	return 0;
 }
