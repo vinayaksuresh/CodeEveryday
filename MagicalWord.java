a51b336aefcedae17cb87d6fbe0f4f30dd1588bb
Vinayak Suresh <vinayaksuresh.india@gmail.com>
2017-01-05 21:52:59 +0530

    Partial Output

--- MagicalWord.java
+++ MagicalWord.java
@@ -1,8 +1,41 @@
-import java.io.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+/*
+https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/magical-word/
+Dhananjay has recently learned about ASCII values.He is very fond of experimenting. With his knowledge of ASCII values and character he has developed a special word and named it Dhananjay's Magical word.
 
-//import for Scanner and other utility classes
+A word which consist of alphabets whose ASCII values is a prime number is an Dhananjay's Magical word. An alphabet is Dhananjay's Magical alphabet if its ASCII value is prime.
+
+Dhananjay's nature is to boast about the things he know or have learnt about. So just to defame his friends he gives few string to his friends and ask them to convert it to Dhananjay's Magical word. None of his friends would like to get insulted. Help them to convert the given strings to Dhananjay's Magical Word.
+
+Rules for converting:
+
+1.Each character should be replaced by the nearest Dhananjay's Magical alphabet.
+
+2.If the character is equidistant with 2 Magical alphabets. The one with lower ASCII value will be considered as its replacement.
+
+Input format:
+
+First line of input contains an integer T number of test cases. Each test case contains an integer N (denoting the length of the string) and a string S.
+
+Output Format:
+
+For each test case, print Dhananjay's Magical Word in a new line.
+
+Constraints:
+
+1 <= T <= 100
+
+1 <= |S| <= 500
+
+SAMPLE INPUT
+1
+6
+AFREEN
+
+SAMPLE OUTPUT
+CGSCCO
+
+*/
+import java.io.*;
 import java.util.*;
 
 class TestClass {
@@ -15,22 +48,37 @@ class TestClass {
     return true;
 }
 
-    static long int calculateNearPrime(long int num) {
-        long int tempNum1 = 0, tempNum2 = 0, BeforeCount = 0, AfterCount = 0;
+    static int calculateNearPrime(int num) {
+        int tempNum1 = 0, tempNum2 = 0, BeforeCount = 0, AfterCount = 0;
         for(int j=num-1;j<num;j--) {
+            if (j>49) {
             BeforeCount++;
             if(isPrime(j)) {
                 tempNum1 = j;
                 break;
             }
-        }
-        for(int j=num+1;j>num;j++) {
-            AfterCount++;
-            if(isPrime(j)) {
-                tempNum2 = j;
+            }
+            else
+            {
+                BeforeCount = 9999;
                 break;
             }
         }
+        for(int j=num+1;j>num;j++) {
+            if (j<126) {
+                AfterCount++;
+                if(isPrime(j)) {
+                    tempNum2 = j;
+                    break;
+                }
+            }
+            else {
+                AfterCount = 9999;
+                break;
+            }
+
+            }
+
         if(BeforeCount<=AfterCount) {
             return tempNum1;
         }
@@ -40,21 +88,21 @@ class TestClass {
 
     public static void main(String args[] ) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
         while (T>0) {
-            long int wordLength = Integer.parseInt(br.readLine());
-            long int[] ASCIIword = new int[wordLength];
-            String tempArray = br.readLine();
-            for(long int i=0;i<wordLength;i++) {
-                ASCIIword[i] = (long int)(tempArray.charAt(i));
-                if (!isPrime(ASCIIword[i])) {
-                    ASCIIword[i] = calculateNearPrime(ASCIIword[i]);
+            int wordLength = Integer.parseInt(br.readLine());
+            int[] ASCIIword = new int[wordLength];
+            char[] word = new char[wordLength];
+            br.read(word);
+            br.readLine();
+            for(int i=0;i<wordLength;i++) {
+                ASCIIword[i] = (int)word[i];
+                    if (!isPrime(ASCIIword[i])) {
+                        ASCIIword[i] = calculateNearPrime(ASCIIword[i]);
+                        word[i] = (char)ASCIIword[i];
+                    }
                 }
-            }
-            for(int j=0;j<wordLength;j++){
-                System.out.print((char)ASCIIword[j]);
-            }
-            System.out.println();
+            System.out.println(word);
             T--;
         }
     }
