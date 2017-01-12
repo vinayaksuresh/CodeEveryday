8d6b6ce60700e11273af95549764c1f0a6e0b803
Vinayak Suresh <vinayaksuresh.india@gmail.com>
2017-01-12 21:47:21 +0530

    HackerRank AI
    A better Algorithm. Still not the best!

--- botClean.c
+++ botClean.c
@@ -12,7 +12,7 @@ void next_move(int posr, int posc, char board[5][5]) {
     int cornerCount=0;
     int LoopCount=0;
     int defaultMove = defaultM(posr, posc);
-    while(cornerCount<4&&LoopCount<15){
+    while(cornerCount<4&&LoopCount<100){
             if(board[posr+1][posc] == 'd') {
                 printf("DOWN\n");
                 printf("CLEAN\n");
@@ -37,32 +37,44 @@ void next_move(int posr, int posc, char board[5][5]) {
                 board[posr][posc-1] = '-';
                 posc = posc-1;
             }
-            else if(defaultMove==1&&posr!=4) {
-               printf("RIGHT\n");
-               posc = posc+1;
-            }
-            else if(defaultMove==0&&posr!=0) {
-               printf("LEFT\n");
-                posc = posc-1;
-            }
             else if(posr==0&&posc==0) {
                 printf("DOWN\n");
                 posr = posr+1;
                 cornerCount++;
             }
-            else if(posr==4&&posc==4) {
-
+            else if(posr==4&&posc==0) {
+                printf("RIGHT\n");
+                posr = posc+1;
                 cornerCount++;
             }
-            else if(posr==4&&posc==0) {
-                printf("UP");
+            else if(posr==4&&posc==4) {
+                printf("UP\n");
                 posr = posr-1;
                 cornerCount++;
             }
             else if(posr==0&&posc==4) {
-                printf("DOWN");
-                posr = posr+1;
+                printf("LEFT\n");
+                posc = posc-1;
+                defaultMove=0;
                 cornerCount++;
+            }
+            else if(defaultMove==1&&posc<=4) {
+                if(posc==4) {
+                    //defaultMove=0;
+                }
+                else {
+               printf("RIGHT\n");
+               posc = posc+1;
+            }
+        }
+            else if(defaultMove==0&&posc>=0) {
+                if(posc==0) {
+                    //defaultMove=1;
+                }
+                else {
+                posc = posc-1;
+                printf("LEFT\n");
+                }
             }
             LoopCount++;
     }
