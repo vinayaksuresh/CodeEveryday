dbe002046a0e7ca16fe01a7c79d22fa73a621c68
Vinayak Suresh <vinayaksuresh.india@gmail.com>
2017-01-15 22:19:55 +0530

    CodeChef

--- SebiAndThehighway.java
+++ SebiAndThehighway.java
@@ -8,24 +8,19 @@ public class Main {
     while(TestCase>0) {
       String[] tempArray =br.readLine().split(" ");
 
-        int S = Integer.parseInt(tempArray[0]);
-        int SG = Integer.parseInt(tempArray[1]);
-        int FG = Integer.parseInt(tempArray[2]);
-        int D = Integer.parseInt(tempArray[3]);
-        int T = Integer.parseInt(tempArray[4]);
+        double S = Double.parseDouble(tempArray[0]);
+        double SG = Double.parseDouble(tempArray[1]);
+        double FG = Double.parseDouble(tempArray[2]);
+        double D = Double.parseDouble(tempArray[3]);
+        double T = Double.parseDouble(tempArray[4]);
 
         double DistanceInKm = (D*50)/1000;
-        System.out.println(DistanceInKm);
         double TimeInMins = T/60;
-        System.out.println(TimeInMins);
         double SpeedOfOtherCar = DistanceInKm*TimeInMins*60;
-        System.out.println(SpeedOfOtherCar);
         double Speed = S+SpeedOfOtherCar;
-        System.out.println(Speed);
 
         double SpeedDiffOfSebi = java.lang.Math.abs(Speed-SG);
         double SpeedDiffOfDad = java.lang.Math.abs(Speed-FG);
-        System.out.println(SpeedDiffOfDad+" "+SpeedDiffOfSebi);
 
         if(SpeedDiffOfDad>SpeedDiffOfSebi) {
           System.out.println("SEBI");
