10d3ebe7887d42bcedfd9805e3df64bbbc201ea8
Vinayak Suresh <vinayaksuresh.india@gmail.com>
2020-12-23 22:37:24 +0530

    Update BuySellStock||.py

--- BuySellStock||.py
+++ BuySellStock||.py
@@ -42,8 +42,8 @@ class Solution(object):
             return 0;
         
         max_profit = 0
-
-        for i in range(0, len(prices)):
+        
+        for i in range(1, len(prices)):
             if (prices[i] > prices[i-1]):
                 max_profit += prices[i]-prices[i-1]
         return max_profit;
