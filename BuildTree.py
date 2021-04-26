7cc2273d629a2738c79a4f372757a893bd758959
Vinayak Suresh <vinayaksuresh.india@gmail.com>
2021-04-26 21:58:33 +0530

    Refactor BuildTree.py

--- BuildTree.py
+++ BuildTree.py
@@ -8,34 +8,34 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
-    
+
     def get_root(self, nodes, pre_order):
         for each in pre_order:
             try:
                 index = nodes.index(each)
                 return index, each
-            except:
+            except Exception:
                 continue
-    
-    def treeify(self, rootNode, nodes, preorder):
+
+    def treeify(self, nodes, preorder):
         root_index, root_value = self.get_root(nodes, preorder)
         root = TreeNode(root_value)
-        
+
         left_tree = nodes[:root_index]
         right_tree = nodes[root_index+1:]
-        
+
         if len(left_tree) > 0:
-            root.left = self.treeify(root, left_tree, preorder)
-        
+            root.left = self.treeify(left_tree, preorder)
+
         if len(right_tree) > 0:
-            root.right = self.treeify(root, right_tree, preorder)
+            root.right = self.treeify(right_tree, preorder)
 
         return root
-    
+
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if len(preorder) == 0 or preorder[0] == -1:
             return TreeNode(val=-1)
-        
-        return self.treeify(TreeNode(preorder[0]), inorder, preorder)
-        
+
+        return self.treeify(inorder, preorder)
